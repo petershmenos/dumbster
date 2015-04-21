0168fa12120b562113709d5a21086ee78d1c5a29
John Baker <baker32@gelib-057-11.ews.illinois.edu>
2015-04-21 13:09:38 -0500

    added new tests

---
+++ test-src/com/dumbster/smtp/NewTestCases.java
@@ -0,0 +1,30 @@
+package com.dumbster.smtp;
+import org.junit.*;
+import com.dumbster.smtp.MailMessage;
+import static org.junit.Assert.*;
+
+public class MailMessageTest {
+
+    private MailMessage message;
+
+    @Before
+    public void setup() {
+        this.message = new MailMessageImpl();
+    }
+
+    @Test
+    public void testMaxHeaders() {
+        message.addHeader("header1", "value1");
+	message.addHeader("header2", "value2");
+	message.addHeader("header3", "value3");
+	message.addHeader("header4", "value4");
+	message.addHeader("header5", "value5");
+	message.addHeader("header6", "value6");
+	message.addHeader("header7", "value7");
+	message.addHeader("header8", "value8");
+	message.addHeader("header9", "value9");
+	message.addHeader("header10", "value10");
+	assertEquals("header1: value1\n\n\n", message.toString());
+    }
+
+}
