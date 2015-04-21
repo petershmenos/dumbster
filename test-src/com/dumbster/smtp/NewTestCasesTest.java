1852278e84dd5c74d6c1c463720d95a8e9b4c84a
petershmenos <baker32@illinois.edu>
2015-04-21 17:24:25 -0500

    bug2

--- test-src/com/dumbster/smtp/NewTestCasesTest.java
+++ test-src/com/dumbster/smtp/NewTestCasesTest.java
@@ -28,39 +28,48 @@ public class NewTestCasesTest {
 
     @Test
     public void testMaxHeaders() {
-        message.addHeader("header1", "value1");
-	message.addHeader("header2", "value2");
-	message.addHeader("header3", "value3");
-	message.addHeader("header4", "value4");
-	message.addHeader("header5", "value5");
-	message.addHeader("header6", "value6");
-	message.addHeader("header7", "value7");
-	message.addHeader("header8", "value8");
-	message.addHeader("header9", "value9");
-	message.addHeader("header10", "value10");
-	Iterator<String> it = message.getHeaderNames();
-	int i = 0;
-	while(it.hasNext()) { i++; it.next(); }
-	assertEquals(10, i);
+	    message.addHeader("header1", "value1");
+		message.addHeader("header2", "value2");
+		message.addHeader("header3", "value3");
+		message.addHeader("header4", "value4");
+		message.addHeader("header5", "value5");
+		message.addHeader("header6", "value6");
+		message.addHeader("header7", "value7");
+		message.addHeader("header8", "value8");
+		message.addHeader("header9", "value9");
+		message.addHeader("header10", "value10");
+		Iterator<String> it = message.getHeaderNames();
+		int i = 0;
+		while(it.hasNext()) { i++; it.next(); }
+		assertEquals(10, i);
     }
 
     @Test
     public void testMaxHeadersPlus1() {
-        message.addHeader("header1", "value1");
-	message.addHeader("header2", "value2");
-	message.addHeader("header3", "value3");
-	message.addHeader("header4", "value4");
-	message.addHeader("header5", "value5");
-	message.addHeader("header6", "value6");
-	message.addHeader("header7", "value7");
-	message.addHeader("header8", "value8");
-	message.addHeader("header9", "value9");
-	message.addHeader("header10", "value10");
-	message.addHeader("header11", "value11");
-	Iterator<String> it = message.getHeaderNames();
-	int i = 0;
-	while(it.hasNext()) { i++; it.next(); }
-	assertEquals(11, i);
+	    message.addHeader("header1", "value1");
+		message.addHeader("header2", "value2");
+		message.addHeader("header3", "value3");
+		message.addHeader("header4", "value4");
+		message.addHeader("header5", "value5");
+		message.addHeader("header6", "value6");
+		message.addHeader("header7", "value7");
+		message.addHeader("header8", "value8");
+		message.addHeader("header9", "value9");
+		message.addHeader("header10", "value10");
+		message.addHeader("header11", "value11");
+		Iterator<String> it = message.getHeaderNames();
+		int i = 0;
+		while(it.hasNext()) { i++; it.next(); }
+		assertEquals(11, i);
+    }
+
+    @Test
+    public void testAppendBadHeader() {
+    	message.addHeader("foo", "bar");
+    	message.addHeader("tim", "tam");
+    	message.addHeader("zing", "zang");
+    	message.appendHeader("tim", " tum");
+    	assertEquals("tam tum", message.getFirstHeaderValue("tim"));
     }
 
     /* Server Options Utests */
