9c9e7a1d3541bf2c015aecf662b20728132a0525
petershmenos <baker32@illinois.edu>
2015-05-08 17:45:22 -0500

    run with

--- test-src/com/dumbster/smtp/MockitoTestCases.java
+++ test-src/com/dumbster/smtp/MockitoTestCases.java
@@ -12,9 +12,12 @@ import java.util.Date;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;  
 
-import static org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mock;   
+import static org.mockito.Mockito.*; 
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoTestCases {
