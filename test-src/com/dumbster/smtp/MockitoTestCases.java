3491cf4656d593c5fd90354f60dc394904997768
petershmenos <baker32@illinois.edu>
2015-05-08 16:41:36 -0500

    Added Mockito Test Cases WIP

--- test-src/com/dumbster/smtp/MockitoTestCases.java
+++ test-src/com/dumbster/smtp/MockitoTestCases.java
@@ -1,21 +1,33 @@
 package com.dumbster.smtp;
 
+import com.dumbster.smtp.SmtpServer;
+import com.dumbster.smtp.MailMessage;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MockitoTestCases() {
 
+	private SmtpServer server;
+
 	@Before
 	public void setup(){
-
+		server = mock(SmtpServer.class);
 	}
 
 	@Test
 	public void mockServer() {
-
+		assertEquals(0, server.getEmailCount());
 	}
 
-	
+	/* Helpers */
+
+	private void setupMockServerTest() {
+		when(server.getEmailCount().thenReturn(0));
+	}
+
 }
