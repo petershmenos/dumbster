6eca7f7361b9f3386197cdb93fc07fdde506ccd9
petershmenos <baker32@illinois.edu>
2015-05-08 16:27:42 -0500

    Added Mockito Test Cases WIP

---
+++ test-src/com/dumbster/smtp/MockitoTestCases.java
@@ -0,0 +1,21 @@
+package com.dumbster.smtp;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.*;
+
+public class MockitoTestCases() {
+
+	@Before
+	public void setup(){
+
+	}
+
+	@Test
+	public void mockServer() {
+
+	}
+
+	
+}
